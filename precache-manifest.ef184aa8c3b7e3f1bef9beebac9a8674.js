c9705357790efa9fcacd665b3f825bd73263ef91
Code Sandbox <hello@codesandbox.io>
2021-07-30 14:03:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ef184aa8c3b7e3f1bef9beebac9a8674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ba5ba3b705aa73f16ad",
+    "url": "https://iaminsidegit.github.io/csb-heqfc/static/css/main.02fb4c32.chunk.css"
+  },
+  {
+    "revision": "3ba5ba3b705aa73f16ad",
+    "url": "https://iaminsidegit.github.io/csb-heqfc/static/js/main.3ba5ba3b.chunk.js"
+  },
+  {
+    "revision": "2d9ea038455099ba599e",
+    "url": "https://iaminsidegit.github.io/csb-heqfc/static/js/1.2d9ea038.chunk.js"
+  },
+  {
+    "revision": "f9997520bd05c2fbc1f6",
+    "url": "https://iaminsidegit.github.io/csb-heqfc/static/js/runtime~main.f9997520.js"
+  },
+  {
+    "revision": "5faade07fc3eef38f581e2894a58d55c",
+    "url": "https://iaminsidegit.github.io/csb-heqfc/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://iaminsidegit.github.io/csb-heqfc/precache-manifest.ef184aa8c3b7e3f1bef9beebac9a8674.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://iaminsidegit.github.io/csb-heqfc/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
